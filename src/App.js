73e310b14cf76e9388f2ca5d71c65683e3e1b973
Christopher Ksiazak <cksiazak@gmail.com>
2019-08-18 16:56:36 -0600

    added new component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import NavBar from './Components/layout/NavBar';
 import './App.css';
 
 class App extends Component {
   render() {
+    const name = 'John Doe';
+
     return (
       <div className="App">
-        <h1>Hello from React</h1>
+        <NavBar />
       </div>
     );
   }
